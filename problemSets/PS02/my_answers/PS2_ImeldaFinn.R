dc4c5d147d1813ee34ddcb2ed2f0dc960e0995bd
I-finn <52965485+I-finn@users.noreply.github.com>
2023-02-16 18:34:47 +0000

    ps2 q1

--- problemSets/PS02/my_answers/PS2_ImeldaFinn.R
+++ problemSets/PS02/my_answers/PS2_ImeldaFinn.R
@@ -1,11 +1,21 @@
+# Thu Feb 16 17:17:07 2023 ------------------------------
+#Imelda Finn
+# POP77004
+# Problem Set 2
+
 #####################
-# load libraries
-# set wd
 # clear global .envir
+# set wd
+# load libraries
 #####################
 
 # remove objects
 rm(list=ls())
+options(prompt = "# ")
+
+# set wd for current folder
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 # detach all libraries
 detachAllPackages <- function() {
   basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
@@ -18,23 +28,45 @@ detachAllPackages()
 # load libraries
 pkgTest <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
-  if (length(new.pkg)) 
+  if (length(new.pkg))
     install.packages(new.pkg,  dependencies = TRUE)
   sapply(pkg,  require,  character.only = TRUE)
 }
 
-# here is where you load any necessary packages
-# ex: stringr
-# lapply(c("stringr"),  pkgTest)
-
-lapply(c(),  pkgTest)
-
-# set wd for current folder
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#genl
+lapply(c("ggplot2", "tidyverse", "stargazer"),  pkgTest)
 
 #####################
 # Problem 1
 #####################
-
 # load data
 load(url("https://github.com/ASDS-TCD/StatsII_Spring2023/blob/main/datasets/climateSupport.RData?raw=true"))
+dataPath <- "./data/ClimateSupport.RDS"
+saveRDS(climateSupport, dataPath)
+
+summary(climateSupport)
+#factors
+climateSupport$choice <- ifelse(climateSupport$choice=="Support", TRUE, FALSE)
+climateSupport$countries <- ifelse(climateSupport$countries=="20 of 192", 0, 
+                                ifelse(climateSupport$countries=="80 of 192", 1, 2))
+climateSupport$sanctions <- ifelse(climateSupport$sanctions=="None", 0, 
+                                ifelse(climateSupport$sanctions=="5%", 1, 
+                                       ifelse(climateSupport$sanctions=="15%", 2, 3)))
+
+
+mod <- glm(choice ~., family = binomial(link="logit"), 
+           data = climateSupport)
+
+stargazer(mod, type="text")
+
+null_mod <- glm(choice ~1, family = binomial(link="logit"), 
+           data = csFacs)
+anova(mod, null_mod)
+#Analysis of Deviance Table
+
+#Model 1: choice ~ countries + sanctions
+#Model 2: choice ~ 1
+#Resid. Df Resid. Dev Df Deviance
+#1      8497          0            
+#2      8499      11783 -2   -11783
+
